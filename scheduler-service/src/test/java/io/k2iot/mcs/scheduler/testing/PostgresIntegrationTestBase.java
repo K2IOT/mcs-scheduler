ad464b8a4355e9eddaf1d06210e7d7613d6676e3
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-06 15:48:50 +0700

    fix: declare Spring Boot configuration for migration IT

--- scheduler-service/src/test/java/io/k2iot/mcs/scheduler/testing/PostgresIntegrationTestBase.java
+++ scheduler-service/src/test/java/io/k2iot/mcs/scheduler/testing/PostgresIntegrationTestBase.java
@@ -1,5 +1,6 @@
 package io.k2iot.mcs.scheduler.testing;
 
+import io.k2iot.mcs.scheduler.SchedulerApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,6 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest(
+    classes = SchedulerApplication.class,
     properties = {
       "spring.autoconfigure.exclude=org.springframework.boot.quartz.autoconfigure.QuartzAutoConfiguration,org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration",
       "spring.flyway.enabled=true"
