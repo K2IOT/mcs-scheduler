fcb7938413eceb7513b303c914188017ec58efd8
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-06 16:09:27 +0700

    feat: add calendar interval trigger specification

---
+++ scheduler-service/src/main/java/io/k2iot/mcs/scheduler/trigger/CalendarIntervalTriggerSpec.java
@@ -0,0 +1,16 @@
+package io.k2iot.mcs.scheduler.trigger;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public record CalendarIntervalTriggerSpec(int interval, ChronoUnit unit) implements TriggerSpec {
+
+    public CalendarIntervalTriggerSpec {
+        Objects.requireNonNull(unit, "unit");
+    }
+
+    @Override
+    public TriggerDefinition.Type type() {
+        return TriggerDefinition.Type.CALENDAR_INTERVAL;
+    }
+}
