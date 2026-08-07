d88043d186e796b3e439b7f94db7f5b1d91f16f4
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-07 16:04:07 +0700

    feat: delegate non-concurrent quartz execution recording

--- scheduler-service/src/main/java/io/k2iot/mcs/scheduler/quartz/NonConcurrentDispatchQuartzJob.java
+++ scheduler-service/src/main/java/io/k2iot/mcs/scheduler/quartz/NonConcurrentDispatchQuartzJob.java
@@ -1,6 +1,12 @@
 package io.k2iot.mcs.scheduler.quartz;
 
+import io.k2iot.mcs.scheduler.execution.ScheduledExecutionService;
 import org.quartz.DisallowConcurrentExecution;
 
 @DisallowConcurrentExecution
-public final class NonConcurrentDispatchQuartzJob extends ConcurrentDispatchQuartzJob {}
+public final class NonConcurrentDispatchQuartzJob extends ConcurrentDispatchQuartzJob {
+
+  public NonConcurrentDispatchQuartzJob(ScheduledExecutionService scheduledExecutionService) {
+    super(scheduledExecutionService);
+  }
+}
