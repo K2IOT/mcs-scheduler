502f2425a82bbd5d116b61951ae084c55afdde00
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-07 09:30:19 +0700

    fix: condition schedule REST endpoint on command facade

--- scheduler-service/src/main/java/io/k2iot/mcs/scheduler/rest/ScheduleController.java
+++ scheduler-service/src/main/java/io/k2iot/mcs/scheduler/rest/ScheduleController.java
@@ -5,6 +5,7 @@ import io.k2iot.mcs.scheduler.command.SchedulerCommands;
 import java.net.URI;
 import java.util.Objects;
 import java.util.UUID;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConditionalOnBean(SchedulerCommandFacade.class)
 @RequestMapping("/api/v1/schedules")
 public final class ScheduleController {
 
