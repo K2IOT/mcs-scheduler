1de02982b86d96718e169cc9ea6c0c00786d32fe
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-07 09:30:06 +0700

    fix: condition trigger REST endpoints on command facade

--- scheduler-service/src/main/java/io/k2iot/mcs/scheduler/rest/TriggerController.java
+++ scheduler-service/src/main/java/io/k2iot/mcs/scheduler/rest/TriggerController.java
@@ -5,6 +5,7 @@ import io.k2iot.mcs.scheduler.trigger.TriggerDefinition;
 import java.net.URI;
 import java.util.Objects;
 import java.util.UUID;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConditionalOnBean(SchedulerCommandFacade.class)
 @RequestMapping("/api/v1/triggers")
 public final class TriggerController {
 
