12f257541e3390510da7014b1654ed463dd625fa
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-08 20:30:35 +0700

    test: isolate grpc port in application context smoke test

--- scheduler-service/src/test/java/io/k2iot/mcs/scheduler/SchedulerApplicationTest.java
+++ scheduler-service/src/test/java/io/k2iot/mcs/scheduler/SchedulerApplicationTest.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
     properties = {
-      "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.springframework.boot.quartz.autoconfigure.QuartzAutoConfiguration,org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration"
+      "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.springframework.boot.quartz.autoconfigure.QuartzAutoConfiguration,org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration",
+      "spring.grpc.server.port=0"
     })
 class SchedulerApplicationTest {
 
