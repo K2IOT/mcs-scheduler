0006e6bbf45b09e1dad0bbbb6aaa29874633b35b
KuuKuu <42238907+VODINHQUOC@users.noreply.github.com>
2026-08-08 16:23:28 +0700

    fix: register REST controllers after command facade

---
+++ scheduler-service/src/main/java/io/k2iot/mcs/scheduler/rest/SchedulerRestAutoConfiguration.java
@@ -0,0 +1,37 @@
+package io.k2iot.mcs.scheduler.rest;
+
+import io.k2iot.mcs.scheduler.command.SchedulerCommandAutoConfiguration;
+import io.k2iot.mcs.scheduler.command.SchedulerCommandFacade;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration(after = SchedulerCommandAutoConfiguration.class)
+@ConditionalOnBean(SchedulerCommandFacade.class)
+public class SchedulerRestAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  ScheduleController scheduleController(SchedulerCommandFacade facade, RestCommandMapper mapper) {
+    return new ScheduleController(facade, mapper);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  JobController jobController(SchedulerCommandFacade facade, RestCommandMapper mapper) {
+    return new JobController(facade, mapper);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  TriggerController triggerController(SchedulerCommandFacade facade, RestCommandMapper mapper) {
+    return new TriggerController(facade, mapper);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  ExecutionController executionController(SchedulerCommandFacade facade, RestCommandMapper mapper) {
+    return new ExecutionController(facade, mapper);
+  }
+}
